dca2b85eb6a220e4939ce350e1b7dbc44b06d153
joy-rout <jamejon15@gmail.com>
2022-07-10 17:01:37 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Switch , Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>App</div>
+    <Switch>
+      <Route exact path="/">This is the home page</Route>
+
+      <Route exact path="/index">This is index page</Route>
+
+      <Route>404 error page not found</Route>
+    </Switch>
   )
 }
 
