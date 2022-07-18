b54845d3ad3da3b21f89b5563f8c60fb7cd2916b
joy-rout <jamejon15@gmail.com>
2022-07-18 14:20:03 +0530

    created show components and populated them

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/function-component-definition */
 import React,{ useEffect , useReducer} from 'react'
 import { useParams } from 'react-router'
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../Misc/Config'
 
 const reducer = (prevState, action) => {
@@ -62,6 +67,39 @@ const reducer = (prevState, action) => {
       return <div>Error occured: {error}</div>;
     }
   
-    return <div>this is show page</div>;
+    return <div>
+        <ShowMainData
+            image={show.image}
+            name={show.name}
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genres}
+        />
+
+        <div>
+            <h2>Details</h2>
+            <Details
+                status={show.status}
+                network={show.network}
+                premiered={show.premiered}
+            />
+        </div>
+
+        
+        <div>
+            <h2>Seasons</h2>
+            <Seasons
+                seasons={show._embedded.seasons}
+            />
+        </div>
+
+        
+        <div>
+            <h2>cast</h2>
+            <Cast
+                cast={show._embedded.cast}
+            />
+        </div>
+    </div>;
   };
 export default Show
