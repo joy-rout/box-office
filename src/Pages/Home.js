f0ce62306452c46d98ca206101f78c0987432e6f
joy-rout <jamejon15@gmail.com>
2022-07-19 14:03:10 +0530

    styled the whole app

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,24 +2,26 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/function-component-definition */
 
-import React,{ useState } from 'react'
-import MainPageLayout from '../components/MainPageLayout'
-import {apiGet} from '../Misc/Config'
-import ShowGrid from '../components/show/ShowGrid'
-import ActorGrid from '../components/actor/ActorGrid'
-
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../Misc/Config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+import {
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+  SearchInput,
+} from './Home.styled';
 
 const Home = () => {
-
-  const [input , setInput ] = useState('');
-  const [results , setResults ] = useState(null);
-  const [searchOption , setSearchOption] = useState('shows');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearched = searchOption === 'shows';
 
   const onSearch = () => {
-    apiGet(`/search/${searchOption}?q=${input}`)
-    .then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -34,68 +36,71 @@ const Home = () => {
     }
   };
 
-  const onRadioChange = (ev) => {
-    setSearchOption(ev.target.value)
-  }
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
 
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div>No Results</div>;
     }
 
-    if( results && results.length > 0) {
-      return results[0].show
-        ? ( <ShowGrid data={results}/> )
-        : ( <ActorGrid data={results}/>)
-        } 
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
+    }
 
     return null;
   };
 
   return (
     <MainPageLayout>
-      <input 
-        type="text" 
-        placeholder='search'
-        onChange={onInputChange} 
-        onKeyDown={onKeyDown} 
+      <SearchInput
+        type="text"
+        placeholder="search"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
         value={input}
       />
-      <div>
-        
-        <label htmlFor='shows-search'>
-          Shows
-          <input 
-            id="shows-search" 
-            type='radio' 
-            value='shows' 
-            checked={isShowsSearched}
-            onChange={onRadioChange}
-          />
-        </label>
 
-        <label htmlFor='actors-search'>
-          Actors
-          <input 
-            id="actors-search" 
-            type='radio' 
-            value='people' 
-            checked={!isShowsSearched}
-            onChange={onRadioChange}
-          />
-        </label>
+      <RadioInputsWrapper>
+        <div>
+          <label htmlFor="shows-search">
+            Shows
+            <input
+              id="shows-search"
+              type="radio"
+              value="shows"
+              checked={isShowsSearched}
+              onChange={onRadioChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="actors-search">
+            Actors
+            <input
+              id="actors-search"
+              type="radio"
+              value="people"
+              checked={!isShowsSearched}
+              onChange={onRadioChange}
+            />
+          </label>
+        </div>
+      </RadioInputsWrapper>
 
-      </div>
-
-      <button 
-        type="button" 
-        onClick={onSearch}
-        >
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
           Search
-      </button>
+        </button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
