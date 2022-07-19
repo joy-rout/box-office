87b4151cc8fb74958594df7593d9902eb45ec33f
joy-rout <jamejon15@gmail.com>
2022-07-19 13:14:07 +0530

    using custom hooks for starred shows

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,25 +1,41 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/function-component-definition */
 
-import React from 'react'
-import ShowCard from './ShowCard'
-import ImageNotFound from '../Images/not-found.png'
-import { FlexGrid } from '../Styled'
+import React from 'react';
+import ShowCard from './ShowCard';
+import ImageNotFound from '../Images/not-found.png';
+import { FlexGrid } from '../Styled';
+import { useShows } from '../../Misc/custom-hooks';
 
-const ShowGrid = ( {data} ) => {
+const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-        {data.map(( {show} ) => (
-            <ShowCard
-                key={show.id}
-                id={show.id}
-                name={show.name}
-                image={show.image ? show.image.medium : ImageNotFound}
-                summary={show.summary}
-            />
-        ))}
-    </FlexGrid>
-  )
-}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
 
-export default ShowGrid
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : ImageNotFound}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
+    </FlexGrid>
+  );
+};
+
+export default ShowGrid;
