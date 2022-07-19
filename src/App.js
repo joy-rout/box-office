eeaed9678fe01163b7228922e0ac6d522ac08393
joy-rout <jamejon15@gmail.com>
2022-07-19 13:49:44 +0530

    added style to navbar and title

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React from 'react'
-import { Switch , Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-import Home from './Pages/Home'
-import Starred from './Pages/Starred'
-import Show from './Pages/Show'
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
+import Show from './Pages/Show';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-  
-    <Switch>
-      <Route exact path="/">
-        <Home/>
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        <Starred/>
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      <Route>
-        <div>404 error page not found</div>
-      </Route>
-    </Switch>
-   
+        <Route>
+          <div>404 error page not found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { useLocation } from 'react-router';
+import { LinkStyled, NavList } from './Navbar.styled';
 
 const LINKS = [
-    { to : '/' , text: 'Home'},
-    { to : '/starred' , text: 'starred'}
+  { to: '/', text: 'Home' },
+  { to: '/starred', text: 'starred' },
 ];
 
-function Navbar() {
-    return (
-        <div>
-            <ul>
-            {
-            LINKS.map(item => (
-            <li key={item.to}>
-              <Link to={item.to}>{item.text}</Link>
-            </li>
-            ))}
-        </ul>
-        </div>
-    )
+const Navbar = () => {
+    const location = useLocation();
+  return (
+    <div>
+      <NavList>
+        {LINKS.map(item => (
+          <li key={item.to}>
+            <LinkStyled 
+                to={item.to}
+                className={item.to === location.pathname ? 'active' : ''}
+                >{item.text}
+                </LinkStyled>
+          </li>
+        ))}
+      </NavList>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
